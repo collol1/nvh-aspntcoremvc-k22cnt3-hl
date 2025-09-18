468441fd49d21953adb1fa26de8769fcd8c58059
collol <sos142004@gmail.com>
2025-09-18 18:02:31 +0700

    update Day02

---
+++ NvhDay02/NvhDay02/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NvhDay02.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAllProducts()
+        {
+            List<Models.Product> products = new List<Models.Product>
+            {
+                new Models.Product { Id = 1, Name = "Product A", Price = 10.0m },
+                new Models.Product { Id = 2, Name = "Product B", Price = 20.0m },
+                new Models.Product { Id = 3, Name = "Product C", Price = 30.0m }
+            };
+            ViewBag.products = products;
+            // trả về view có tên là Products trong thư mục Views/Product
+            return View("Products");
+        }
+       public IActionResult GetProduct()
+        {
+            Models.Product p = new Models.Product
+            {
+                Id = 1,
+                Name = "Product A",
+                Price = 10.0m
+            };
+            ViewBag.products = p;
+            ViewData["productsvd"] = p;
+            return View();
+        }
+    }
+}
---
+++ NvhDay02/NvhDay02/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NvhDay02.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductController : Controller
+    {
+        public IActionResult GetProduct()
+        {
+            Product p = new Product
+            {
+                Id = 1,
+                Name = "Product A",
+                Price = 10.0m
+            };
+            ViewBag.products = p;
+            ViewData["productsvd"] = p;
+            return View();
+        }
+    }
+}
