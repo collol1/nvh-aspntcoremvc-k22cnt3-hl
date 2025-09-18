4427637ca3490b8b667732b0bda7c730f7465f59
collol <sos142004@gmail.com>
2025-09-18 15:41:01 +0700

    update Day01

---
+++ NvhDay01/NvhDay01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NvhDay01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
